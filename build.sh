179701e036909f5fc193f23d100978ba9c7f4d2d
Tomas Kirnak <tomas@unimus.net>
2019-02-28 19:50:23 +0100

    Added a build script which can build final scripts for testing or prod

---
+++ build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Version: 2019-02-28-01
+
+# set workdir to the script dir
+cd "$(dirname "$0")"
+
+# make target directory if missing
+if [ ! -d "target" ]; then
+  mkdir "target";
+fi
+
+# target cleanup
+rm -r target/*;
+
+echo "Select build profile:";
+options=( "test" "prod" "Quit" );
+
+select opt in "${options[@]}"; do
+  case $REPLY in
+    1) import_command="./%s";;
+    2) import_command="bash <(curl -sS 'https://unimus.net/download/linux-v2/%s')";;
+    3) break;;
+  esac
+done
+
+cp -r src/* target;
+cd target;
+
+find . -type f -exec sed -i -r "s#<import-replace\|(.+?)\|import-replace>#$(printf "${import_command}" "\1")#" {} +;
