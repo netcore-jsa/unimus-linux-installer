61a52c2f820347b462b9c96d77cce8ecc83940de
Tomas Kirnak <tomas@unimus.net>
2019-03-02 19:23:56 +0100

    openjdk-r packaged not installable on Debian, switching to Oracle Java repo for older Debian distros

--- src/params/debian-raspbian-ubuntu.sh
+++ src/params/debian-raspbian-ubuntu.sh
@@ -16,43 +16,58 @@ service_autostart_add_command='update-rc.d %s defaults';
 service_autostart_remove_command='update-rc.d -f %s remove';
 
 function add_java_package_repo {
-  echo "You OS doesn't have the required Java version in its default APT repositories.";
-  echo 'Would you like the installer to add the latest OpenJDK APT repo to your system?';
-  echo;
-  echo 'Press ENTER to continue, or Ctrl+C to exit:';
-  read -s;
-  echo;
-
   case $os_release in
     *"Ubuntu"*)
-      # make sure we have 'add-apt-repository'
-      $(printf "${package_install_command}" 'add-apt-repository') &> /dev/null;
+      add_ubuntu_openjdk_ppa;;
+    *"Debian"*|*"Raspbian"*)
+      add_debian_oracle_java_repo;;
+    *)
+      echo_no_java_supported_packages;
+      exit 1;;
+  esac;
+}
 
-      # add OpenJDK repo
-      add-apt-repository ppa:openjdk-r/ppa;
-      ;;
-    *"Debian"*)
-      # find the Ubuntu equivalent to this Debian
-      case $os_release in
-        *"jessie"*)
-          ubuntu_equivalent='trusty';;
-        *"wheezy"*)
-          ubuntu_equivalent='precise';;
-        *)
-          echo_no_java_supported_packages;
-          exit 1;;
-      esac;
+function add_ubuntu_openjdk_ppa {
+  echo_java_repo_confirmation 'OpenJDK';
 
-      # add the OpenJDK PPA
-      echo "deb http://ppa.launchpad.net/openjdk-r/ppa/ubuntu ${ubuntu_equivalent} main" > /etc/apt/sources.list.d/openjdk-r.list;
-      echo "deb-src http://ppa.launchpad.net/openjdk-r/ppa/ubuntu ${ubuntu_equivalent} main " >> /etc/apt/sources.list.d/openjdk-r.list;
-      apt-key adv --keyserver keyserver.ubuntu.com --recv-keys DA1A4A13543B466853BAF164EB9B1D8886F44E2A  &> /dev/null;
-      ;;
+  # make sure we have 'add-apt-repository'
+  $(printf "${package_install_command}" 'add-apt-repository') &> /dev/null;
+
+  # add OpenJDK repo
+  add-apt-repository ppa:openjdk-r/ppa;
+
+  echo 'Done, OpenJDK APT repo added to the system.';
+  echo;
+}
+
+function add_debian_oracle_java_repo {
+  echo_java_repo_confirmation 'Oracle Java';
+
+  # find the Ubuntu equivalent to this Debian
+  case $os_release in
+    *"jessie"*)
+      ubuntu_equivalent='trusty';;
+    *"wheezy"*)
+      ubuntu_equivalent='precise';;
     *)
       echo_no_java_supported_packages;
       exit 1;;
   esac;
 
-  echo 'Done, OpenJDK APT repo added to the system.';
+  # add the Oracle Java repo
+  echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu ${ubuntu_equivalent} main" > /etc/apt/sources.list.d/webupd8team-oracle-java.list;
+  echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu ${ubuntu_equivalent} main " >> /etc/apt/sources.list.d/webupd8team-oracle-java.list;
+  apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 7B2C3B0889BF5709A105D03AC2518248EEA14886 &> /dev/null;
+
+  echo 'Done, Oracle Java APT repo added to the system.';
+  echo;
+}
+
+function echo_java_repo_confirmation {
+  echo "You OS doesn't have the required Java version in its default APT repositories.";
+  echo "Would you like the installer to add the latest ${1} APT repo to your system?";
+  echo;
+  echo 'Press ENTER to continue, or Ctrl+C to exit:';
+  read -s;
   echo;
 }
