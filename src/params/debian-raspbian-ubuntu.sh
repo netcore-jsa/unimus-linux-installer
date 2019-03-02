4eb8233b50b4361c8442c3a531f1ca3832786dd2
Tomas Kirnak <tomas@unimus.net>
2019-03-02 20:02:00 +0100

    More update to Ubuntu and Debian when using APT Java repos (fixes multiple 'press enter' requests)

--- src/params/debian-raspbian-ubuntu.sh
+++ src/params/debian-raspbian-ubuntu.sh
@@ -28,20 +28,26 @@ function add_java_package_repo {
 }
 
 function add_ubuntu_openjdk_ppa {
-  echo_java_repo_confirmation 'OpenJDK';
+  local apt_repo_file='/etc/apt/sources.list.d/openjdk-r.list';
 
-  # make sure we have 'add-apt-repository'
-  $(printf "${package_install_command}" 'software-properties-common') &> /dev/null;
+  echo_java_repo_confirmation 'OpenJDK' "${apt_repo_file}";
 
-  # add OpenJDK repo
-  add-apt-repository ppa:openjdk-r/ppa;
+  # make sure 'lsb_release' is available
+  $(printf "${package_install_command}" 'lsb-release') &> /dev/null;
+
+  # add the Oracle Java repo
+  echo "deb http://ppa.launchpad.net/openjdk-r/ppa/ubuntu $(lsb_release -cs) main" > ${apt_repo_file};
+  echo "deb-src http://ppa.launchpad.net/openjdk-r/ppa/ubuntu $(lsb_release -cs) main " >> ${apt_repo_file};
+  apt-key adv --keyserver keyserver.ubuntu.com --recv-keys DA1A4A13543B466853BAF164EB9B1D8886F44E2A &> /dev/null;
 
   echo 'Done, OpenJDK APT repo added to the system.';
   echo;
 }
 
 function add_debian_oracle_java_repo {
-  echo_java_repo_confirmation 'Oracle Java';
+  local apt_repo_file='/etc/apt/sources.list.d/webupd8team-oracle-java.list';
+
+  echo_java_repo_confirmation 'Oracle Java' "${apt_repo_file}";
 
   # find the Ubuntu equivalent to this Debian
   case $os_release in
@@ -55,8 +61,8 @@ function add_debian_oracle_java_repo {
   esac;
 
   # add the Oracle Java repo
-  echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu ${ubuntu_equivalent} main" > /etc/apt/sources.list.d/webupd8team-oracle-java.list;
-  echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu ${ubuntu_equivalent} main " >> /etc/apt/sources.list.d/webupd8team-oracle-java.list;
+  echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu ${ubuntu_equivalent} main" > ${apt_repo_file};
+  echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu ${ubuntu_equivalent} main " >> ${apt_repo_file};
   apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 7B2C3B0889BF5709A105D03AC2518248EEA14886 &> /dev/null;
 
   echo 'Done, Oracle Java APT repo added to the system.';
@@ -66,6 +72,7 @@ function add_debian_oracle_java_repo {
 function echo_java_repo_confirmation {
   echo "You OS doesn't have the required Java version in its default APT repositories.";
   echo "Would you like the installer to add the latest ${1} APT repo to your system?";
+  echo "(will use '${2}')";
   echo;
   echo 'Press ENTER to continue, or Ctrl+C to exit:';
   read -s;
