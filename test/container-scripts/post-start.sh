0fd7c5014e852f8d6b3ba8d7fae0637d5180333b
Tomas Kirnak <tomas@unimus.net>
2020-03-31 01:25:05 +0200

    Improvements to test container start scripts
    Fixes issues on CentOS / AWS Linux

--- test/container-scripts/post-start.sh
+++ test/container-scripts/post-start.sh
@@ -2,18 +2,42 @@
 
 # some containers (especially old ones) need fixing before they are usable
 case $IMAGE in
-  "debian:7")
+  *"centos"*|*"amazonlinux"*)
     echo "Running post-start fixes for '${IMAGE}'";
     echo;
 
-    echo "deb http://archive.debian.org/debian wheezy main" > /etc/apt/sources.list;
+    yum install initscripts -y -q;
+    echo;
     ;;
 esac;
 
+
+
+options=( "Core installer" "Unimus installer" "Shell (bash)" "Quit" );
+
+select opt in "${options[@]}"; do
+  case $REPLY in
+    1) product="core";
+       break;;
+
+    2) product="unimus";
+       break;;
+
+    3) /bin/bash;;
+
+    4) echo;
+       echo "Exiting container...";
+       echo;
+       exit;;
+  esac;
+done;
+
 # run install script
-/root/container-scripts/run-install.sh;
+echo;
+/root/container-scripts/run-install.sh ${product};
 
 # run post-install checks
+echo;
 /root/container-scripts/install-error-check.sh;
 
 # drop the user off in a shell
--- test/container-scripts/run-install.sh
+++ test/container-scripts/run-install.sh
@@ -1,24 +1,6 @@
 #! /bin/bash
 
-options=( "Core installer" "Unimus installer" "Shell (bash)" "Quit" );
+product=$1;
 
-select opt in "${options[@]}"; do
-  case $REPLY in
-    1) product="core";
-       break;;
-
-    2) product="unimus";
-       break;;
-
-    3) /bin/bash;;
-
-    4) echo;
-       echo "Exiting container...";
-       echo;
-       exit;;
-  esac;
-done;
-
-echo;
 echo "Running ${product} installer...";
 /root/${product}-installer/install.sh "${UNATTENDED}" "${DEBUG}" | tee /root/install.log;
