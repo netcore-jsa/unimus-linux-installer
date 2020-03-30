8f0fe4de0d41fb92f531fde0e558e11549324a3a
Tomas Kirnak <tomas@unimus.net>
2020-03-31 01:25:41 +0200

    Fixes and improvements to OpenJDK JRE install on Ubuntu / Debian

--- src/params/debian-raspbian-ubuntu.sh
+++ src/params/debian-raspbian-ubuntu.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Version: 2019-03-03-01
-
 # package manager commands
 package_list_update_command='apt-get update';
 package_check_available_command='apt-cache show';
@@ -17,73 +15,82 @@ java_package_install_list=( 'openjdk-11-jre' 'openjdk-8-jre' );
 service_autostart_add_command='update-rc.d %s defaults';
 service_autostart_remove_command='update-rc.d -f %s remove';
 
-# FIXME: add supported RPi version checking
+# add https transport for APT
+dependency_packages+=( 'apt-transport-https' );
 
 function add_java_package_repo {
   case $os_release in
     *"Ubuntu"*)
       add_ubuntu_openjdk_ppa;;
     *"Debian"*|*"Raspbian"*)
-      add_debian_oracle_java_repo;;
+      add_debian_backports;;
     *)
       echo_no_java_supported_packages;
       exit 1;;
   esac;
+
+  package_list_update;
 }
 
 function add_ubuntu_openjdk_ppa {
-  local apt_repo_file='/etc/apt/sources.list.d/openjdk-r.list';
+  local openjdk_apt_repo_file='/etc/apt/sources.list.d/openjdk-r.list';
 
-  echo_java_repo_confirmation 'OpenJDK' "${apt_repo_file}";
-
-  # make sure 'lsb_release' is available
+  # make sure 'lsb_release' is available, it should be default be on all Debian-based systems
   $(printf "${package_install_command}" 'lsb-release') &> /dev/null;
 
-  # add the Oracle Java repo
-  echo "deb http://ppa.launchpad.net/openjdk-r/ppa/ubuntu $(lsb_release -cs) main" > ${apt_repo_file};
-  echo "deb-src http://ppa.launchpad.net/openjdk-r/ppa/ubuntu $(lsb_release -cs) main " >> ${apt_repo_file};
+  # confirm repo addition
+  echo "Your OS doesn't have the required Java version in it's default APT repositories.";
+  echo "Would you like the installer to add the OpenJDK APT repo to your system?";
+  echo "(will use '${openjdk_apt_repo_file}')";
+  echo;
+
+  if [[ $unattended == 0 ]]; then
+    echo 'Press ENTER to continue, or Ctrl+C to exit:';
+    read -s;
+    echo;
+  fi;
+
+  debug "Adding OpenJDK repo, '$(lsb_release -cs)', '${openjdk_apt_repo_file}'";
+
+  # add the openjdk-r ppa
+  echo "deb http://ppa.launchpad.net/openjdk-r/ppa/ubuntu $(lsb_release -cs) main" > ${openjdk_apt_repo_file};
+  echo "deb-src http://ppa.launchpad.net/openjdk-r/ppa/ubuntu $(lsb_release -cs) main " >> ${openjdk_apt_repo_file};
   apt-key adv --keyserver keyserver.ubuntu.com --recv-keys DA1A4A13543B466853BAF164EB9B1D8886F44E2A &> /dev/null;
 
   echo 'Done, OpenJDK APT repo added to the system.';
   echo;
 }
 
-function add_debian_oracle_java_repo {
-  local apt_repo_file='/etc/apt/sources.list.d/webupd8team-oracle-java.list';
-
-  echo_java_repo_confirmation 'Oracle Java' "${apt_repo_file}";
-
-  # find the Ubuntu equivalent to this Debian
+function add_debian_backports {
   case $os_release in
     *"jessie"*)
-      ubuntu_equivalent='trusty';;
-    *"wheezy"*)
-      ubuntu_equivalent='precise';;
+      os_name='jessie';;
     *)
       echo_no_java_supported_packages;
       exit 1;;
   esac;
 
-  echo 'Adding Oracle Java APT repo to the system.';
+  local backports_repo_file="/etc/apt/sources.list.d/${os_name}-backports.list";
 
-  # add the Oracle Java repo
-  echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu ${ubuntu_equivalent} main" > ${apt_repo_file};
-  echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu ${ubuntu_equivalent} main " >> ${apt_repo_file};
-  apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 7B2C3B0889BF5709A105D03AC2518248EEA14886 &> /dev/null;
-
-  # updated Java package install name for the Oracle Java package
-  java_package_install_list=( 'oracle-java8-installer' );
-
-  echo 'Done, Oracle Java APT repo added to the system.';
+  # confirm backports addition
+  echo "Your OS doesn't have the required Java version in it's default APT repositories.";
+  echo "Would you like the installer to add the 'backports' APT repo to your system?";
+  echo "(will use '${backports_repo_file}')";
   echo;
-}
 
-function echo_java_repo_confirmation {
-  echo "Your OS doesn't have the required Java version in its default APT repositories.";
-  echo "Would you like the installer to add the latest ${1} APT repo to your system?";
-  echo "(will use '${2}')";
-  echo;
-  echo 'Press ENTER to continue, or Ctrl+C to exit:';
-  read -s;
+  if [[ $unattended == 0 ]]; then
+    echo 'Press ENTER to continue, or Ctrl+C to exit:';
+    read -s;
+    echo;
+  fi;
+
+  debug "Adding 'backports' repo, '${os_name}', '${backports_repo_file}'";
+
+  # add the backports repo
+  echo "deb http://archive.debian.org/debian ${os_name}-backports main" > ${backports_repo_file};
+  echo "Acquire::Check-Valid-Until false;" > /etc/apt/apt.conf.d/10-nocheckvalid;
+  echo -e "Package: ca-certificates-java\nPin: origin \"archive.debian.org\"\nPin-Priority: 500" > /etc/apt/preferences.d/10-archive-pin;
+
+  echo "Done, 'backports' APT repo added to the system.";
   echo;
 }
