f92855b0d7b6ccdbd46f4dfdcb35a3180c4c080d
vik-unimus <89800975+vik-unimus@users.noreply.github.com>
2022-01-10 17:21:38 +0100

    Added support for minimal upgrade

--- src/unimus-core/install.sh
+++ src/unimus-core/install.sh
@@ -15,9 +15,23 @@ cd "$(dirname "$0")";
 # default installer behavior
 interactive=1;
 debug=0;
+minimal=0;
 
 # save provided arguments
 run_args=$@;
+
+# verify presence of existing installation if minimal upgrade is chosen
+for i in ${run_args[@]}; do
+	if [[ $i == '-m' ]]; then
+		if [ -f "${binary_path}" ]; then
+			break;
+		else
+			echo 'ERROR: We are sorry, but a minimal upgrade can be run only on existing installations.';
+			echo "Remove \"-m\" argument to run a fresh installation of ${product_name}.";
+			exit 1;
+		fi;
+	fi;
+done
 
 # run install script
 main;
